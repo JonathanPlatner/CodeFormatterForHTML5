49f747bc9b8d5f608f7dc464ff69e7f719084a50
JonathanPlatner <42499453+JonathanPlatner@users.noreply.github.com>
2018-11-24 16:48:04 -0800

    Updated class and keyword definitions

--- codeStyle.js
+++ codeStyle.js
@@ -9,29 +9,37 @@ $(document).ready(function(){
    
 	$('.csharp').each(function(){
 		
-	var text = $(this).text();
+	var text = $(this).html();
 	
 	//text = text.replace(/["'].+["']/g,'<font color="mediumspringgreen">$&</font>');
 	//text = text.replace(/(?<!^)(?<=[^\w])(\d+\.\d+f|\d+\.\d+|\d+f|\d+)/g, '<font color="plum">$&</font>');
 	//text = text.replace(/(?<=class\s)\w+|(?<=:).+(?=\{)/g, '<font color="orange">$&</font>'); //Class
-	text = text.replace(GetKeywords(), '<span class="keyword">$&</span>'); //Accessors
-
+	
+	var comments=CopyComments(text);
+	text = RemoveComments(text);
+	text = text.replace(GetKeywords(), '<span class="keyword">$&</span>');
+	
+	text = text.replace(GetClasses(), '<span class="class">$&</span>'); //Accessors
+	 
+	text = AddComments(text,comments);
+	//text=GetKeywords();
 	//text = text.replace(/\w+(?=\()/g,'<font color="mediumspringgreen">$&</font>');
-	text = text.replace(/(\/\/.+)/g,'<span class="comment">$&</span>');
+	//text = text.replace(/(\/\/.+)/g,'<span class="comment">$&</span>');
 	
 	//text = text.replace(baseTypesRegex,'<font color="deepskyblue">$&</font>');
 	
 	//text = text.replace(userTypesRegex,'<font color="orange">$&</font>');
-	
+	//text=text.split(' ');
+	//text = text.join(' ');
    
-   
-   
-	
+	//text = text.replace(GetKeywords())
+	//text = RemoveComments(text);
+	//text = ReplaceText(text,GetKeywords(),'keyword');
 	
 	//text = text.replace(/\r\n\d+/g, '<font color="yellow">$&</font>');
    
    
-	$(this).test(text);
+	$(this).html(text);
 	});
 
 });
